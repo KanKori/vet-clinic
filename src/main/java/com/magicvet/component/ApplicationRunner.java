eeef3cb6333c5233fdc4507ffe1f0a239edcd9a8
Anastasiia Voshchenko <inavoinavo@gmail.com>
2024-04-28 19:38:27 +0300

    REQ-03-EXTRA: the functionality of checking whether a client needs to register a pet immediately has been implemented.

--- src/main/java/com/magicvet/component/ApplicationRunner.java
+++ src/main/java/com/magicvet/component/ApplicationRunner.java
@@ -1,30 +1,64 @@
 package main.java.com.magicvet.component;
 
+import main.java.com.magicvet.Main;
 import main.java.com.magicvet.model.Client;
 import main.java.com.magicvet.model.Pet;
 import main.java.com.magicvet.service.ClientService;
 import main.java.com.magicvet.service.PetService;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ApplicationRunner {
 
     private final ClientService clientService = new ClientService();
     private final PetService  petService = new PetService();
 
+    private static final String CHOICE_PATTERN = "[yn]";
+
     public void run() {
         if (Authenticator.auth()) {
             Client client = clientService.registerNewClient();
 
             if (client != null) {
-                System.out.println("Adding a new pet");
+                if(ifUserWantsToAddAPet()) {
 
-                Pet pet = petService.registerNewPet();
-                client.setPet(pet);
-                pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
-                System.out.println("Pet has been added.");
+                    System.out.println("Adding a new pet");
+
+                    Pet pet = petService.registerNewPet();
+                    client.setPet(pet);
+                    pet.setOwnerName(client.getFirstName() + " " + client.getLastName());
+                    System.out.println("Pet has been added.");
+
+                }
 
                 System.out.println(client);
             }
         }
+    }
+
+
+    public static boolean ifUserWantsToAddAPet() {
+
+        System.out.println("Do you want to add a pet? (y/n): ");
+        String userChoice = Main.SCANNER.nextLine();
+
+        while (!choiceValid(userChoice)) {
+            System.out.println("Incorrect input. Please, try again (y/n): ");
+            userChoice = Main.SCANNER.nextLine();
+        }
+
+        return userChoice.equals("y");
+
+    }
+
+    public static boolean choiceValid(String name) {
+
+        Pattern pattern = Pattern.compile(CHOICE_PATTERN);
+        Matcher matcher = pattern.matcher(name);
+
+        return matcher.matches();
+
     }
 
 }

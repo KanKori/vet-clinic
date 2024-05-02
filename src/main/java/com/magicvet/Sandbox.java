fcdba41e4fde8f7945c21f131dc7a07dc23f7fb2
Anastasiia Voshchenko <inavoinavo@gmail.com>
2024-05-02 18:58:12 +0300

    REQ-06-EXTRA: sort pets by age

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,15 +1,24 @@
 package main.java.com.magicvet;
 
 import main.java.com.magicvet.comparator.DogSizeComparator;
+import main.java.com.magicvet.comparator.PetAgeComparator;
+import main.java.com.magicvet.model.Cat;
 import main.java.com.magicvet.model.Dog;
+import main.java.com.magicvet.service.PetService;
 
 import java.util.Arrays;
 
 public class Sandbox {
 
+    private static final String DOG_TYPE = "dog";
+    private static final String CAT_TYPE = "cat";
+
     public static void main(String[] args) {
 
+        final PetService petService = new PetService();
+
         Dog[] dogs = {
+
                 new Dog(Dog.XL),
                 new Dog(Dog.L),
                 new Dog(Dog.L),
@@ -22,6 +31,20 @@ public class Sandbox {
 
         for (Dog dog : dogs) {
             System.out.println(dog.getSize());
+        }
+
+        Cat[] cats = {
+                (Cat) petService.buildPet("cat"),
+                (Cat) petService.buildPet("cat"),
+                (Cat) petService.buildPet("cat"),
+                (Cat) petService.buildPet("cat"),
+                (Cat) petService.buildPet("cat")
+        };
+
+        Arrays.sort(cats, new PetAgeComparator());
+
+        for (Cat cat : cats) {
+            System.out.println(cat.getAge());
         }
 
     }

327bec72831ed11391a6e544a1e3a96870b46af0
Anastasiia Voshchenko <inavoinavo@gmail.com>
2024-05-08 15:39:52 +0300

    REQ-07: Use enum for dog size

--- src/main/java/com/magicvet/Sandbox.java
+++ src/main/java/com/magicvet/Sandbox.java
@@ -1,24 +1,29 @@
 package main.java.com.magicvet;
 
-import main.java.com.magicvet.comparator.DogSizeComparator;
 import main.java.com.magicvet.model.Dog;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Sandbox {
 
     public static void main(String[] args) {
 
         Dog[] dogs = {
-                new Dog(Dog.XL),
-                new Dog(Dog.L),
-                new Dog(Dog.L),
-                new Dog(Dog.S),
-                new Dog(Dog.XS)
+                new Dog(Dog.Size.XL),
+                new Dog(Dog.Size.L),
+                new Dog(Dog.Size.L),
+                new Dog(Dog.Size.S),
+                new Dog(Dog.Size.XS)
 
         };
 
-        Arrays.sort(dogs, new DogSizeComparator());
+        Arrays.sort(dogs, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog dog1, Dog dog2) {
+                return dog1.getSize().getValue() - dog2.getSize().getValue();
+            }
+        });
 
         for (Dog dog : dogs) {
             System.out.println(dog.getSize());
